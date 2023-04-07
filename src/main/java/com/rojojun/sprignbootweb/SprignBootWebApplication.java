ebbad81798dbadd41dc3478c2b7ba0bec6ef4b58
Hojun Na <72805486+Rojojun@users.noreply.github.com>
2023-04-07 19:54:09 +0900

    first commit

---
+++ src/main/java/com/rojojun/sprignbootweb/SprignBootWebApplication.java
@@ -0,0 +1,13 @@
+package com.rojojun.sprignbootweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprignBootWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SprignBootWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rojojun/sprignbootweb/SprignBootWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rojojun.sprignbootweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprignBootWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
