9c5566bac611176779a415e6886ec0778e569df9
rojojun <zoloman316@gmail.com>
2023-04-11 23:28:17 +0900

    테스트 코드

--- src/test/java/com/rojojun/sprignbootweb/HelloControllerTest.java
+++ src/test/java/com/rojojun/sprignbootweb/HelloControllerTest.java
@@ -7,6 +7,10 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 @RunWith(SpringRunner.class)
 @WebMvcTest
 public class HelloControllerTest {
@@ -16,5 +20,10 @@ public class HelloControllerTest {
 
     @Test
     public void Test_리턴_성공_테스트코드() throws Exception {
+        String hello = "hello";
+
+        mockMvc.perform(get("/hello"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(hello));
     }
 }
