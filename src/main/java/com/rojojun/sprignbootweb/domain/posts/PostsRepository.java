8c4c54095c3f463a32be1a43c245a122c8087dd4
rojojun <zoloman316@gmail.com>
2023-04-18 23:12:51 +0900

    commit

---
+++ src/main/java/com/rojojun/sprignbootweb/domain/posts/PostsRepository.java
@@ -0,0 +1,6 @@
+package com.rojojun.sprignbootweb.domain.posts;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostsRepository extends JpaRepository<Posts,Long> {
+}
