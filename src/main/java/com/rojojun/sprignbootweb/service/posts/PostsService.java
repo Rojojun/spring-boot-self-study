e6088782a95428e6a949bb231f2cec89aaaace45
rojojun <zoloman316@gmail.com>
2023-04-25 23:49:14 +0900

    commit

--- src/main/java/com/rojojun/sprignbootweb/service/posts/PostsService.java
+++ src/main/java/com/rojojun/sprignbootweb/service/posts/PostsService.java
@@ -1,4 +1,19 @@
 package com.rojojun.sprignbootweb.service.posts;
 
+import com.rojojun.sprignbootweb.domain.posts.PostsRepository;
+import com.rojojun.sprignbootweb.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
 public class PostsService {
+
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
 }
