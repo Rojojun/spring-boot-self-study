dbe47edd5498810db64b56e0c06e3e3a042f4f1c
rojojun <zoloman316@gmail.com>
2023-05-21 19:57:59 +0900

    머스태치 설정을 위한 controller 어노테이션 변경

--- src/main/java/com/rojojun/sprignbootweb/controller/IndexController.java
+++ src/main/java/com/rojojun/sprignbootweb/controller/IndexController.java
@@ -1,9 +1,10 @@
 package com.rojojun.sprignbootweb.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class IndexController {
     @GetMapping("/")
     public String index() {
