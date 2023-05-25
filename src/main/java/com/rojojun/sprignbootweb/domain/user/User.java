b7814890ec2c2603cdca999c88cf1d659557311c
rojojun <zoloman316@gmail.com>
2023-05-25 22:51:46 +0900

    User 객체 생성

---
+++ src/main/java/com/rojojun/sprignbootweb/domain/user/User.java
@@ -0,0 +1,19 @@
+package com.rojojun.sprignbootweb.domain.user;
+
+import com.rojojun.sprignbootweb.domain.BaseTimeEntity;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class User extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+}
