73fbe0f25bd61474091b6c6211e17b909ce14dca
rojojun <zoloman316@gmail.com>
2023-05-26 00:02:58 +0900

    User Entity 클래스 완성

--- src/main/java/com/rojojun/sprignbootweb/domain/user/User.java
+++ src/main/java/com/rojojun/sprignbootweb/domain/user/User.java
@@ -1,13 +1,11 @@
 package com.rojojun.sprignbootweb.domain.user;
 
 import com.rojojun.sprignbootweb.domain.BaseTimeEntity;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Getter
 @NoArgsConstructor
@@ -16,4 +14,37 @@ public class User extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column
+    private String picture;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    @Builder
+    public User(String email, String name, String picture, Role role) {
+        this.email = email;
+        this.name = name;
+        this.picture = picture;
+        this.role = role;
+    }
+
+    public User update(String name, String picture) {
+        this.name = name;
+        this.picture = picture;
+
+        return this;
+    }
+
+    public String getRoleKey() {
+        return this.role.getKey();
+    }
+
 }
