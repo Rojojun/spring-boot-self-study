df4f4a18b3519b12e8beca48b0d7d6cee2fa0416
rojojun <zoloman316@gmail.com>
2023-05-27 11:58:16 +0900

    OAuth2 컨피그 설정

---
+++ src/main/java/com/rojojun/sprignbootweb/config/auth/SpringConfig.java
@@ -0,0 +1,32 @@
+package com.rojojun.sprignbootweb.config.auth;
+
+import com.rojojun.sprignbootweb.domain.user.Role;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+
+@RequiredArgsConstructor
+@EnableWebSecurity
+public class SpringConfig extends WebSecurityConfiguration {
+
+    private final CustomOAuth2UserService customOAuth2UserService;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .headers().frameOptions().disable()
+                .and()
+                .authorizeRequests()
+                .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2/**", "/h2-console/**")
+                .permitAll()
+                .antMatchers("/api/v1/**").hasRole(Role.USER.name())
+                .anyRequest().authenticated()
+                .and()
+                .logout()
+                .logoutSuccessUrl("/")
+                .and()
+                .oauth2Login().userInfoEndpoint().userService(customOAuth2UserService);
+    }
+}
