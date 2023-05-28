1dd84de59d6aa8e1a84020638e3773a99ede7a72
rojojun <zoloman316@gmail.com>
2023-05-28 11:06:56 +0900

    Auth 컨트롤러 구현

--- src/main/java/com/rojojun/sprignbootweb/config/auth/SpringConfig.java
+++ src/main/java/com/rojojun/sprignbootweb/config/auth/SpringConfig.java
@@ -5,10 +5,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @RequiredArgsConstructor
 @EnableWebSecurity
-public class SpringConfig extends WebSecurityConfiguration {
+public class SpringConfig extends WebSecurityConfigurerAdapter {
 
     private final CustomOAuth2UserService customOAuth2UserService;
 
--- src/main/java/com/rojojun/sprignbootweb/controller/IndexController.java
+++ src/main/java/com/rojojun/sprignbootweb/controller/IndexController.java
@@ -1,5 +1,7 @@
 package com.rojojun.sprignbootweb.controller;
 
+import com.rojojun.sprignbootweb.config.auth.dto.SessionUser;
+import com.rojojun.sprignbootweb.domain.user.User;
 import com.rojojun.sprignbootweb.service.posts.PostsService;
 import com.rojojun.sprignbootweb.web.dto.PostsResponseDto;
 import com.rojojun.sprignbootweb.web.dto.PostsUpdateRequestDto;
@@ -8,15 +10,23 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("posts", postsService.findAllDesc());
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
