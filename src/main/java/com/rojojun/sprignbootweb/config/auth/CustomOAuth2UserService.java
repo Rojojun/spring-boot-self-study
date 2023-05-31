d5666103f6da8ea6c7d86295627c570fd96581ca
rojojun <zoloman316@gmail.com>
2023-05-31 19:43:30 +0900

    시큐리티 설정 오류 gradle 추가

--- src/main/java/com/rojojun/sprignbootweb/config/auth/CustomOAuth2UserService.java
+++ src/main/java/com/rojojun/sprignbootweb/config/auth/CustomOAuth2UserService.java
@@ -15,9 +15,7 @@ import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpSession;
-import java.util.Collection;
 import java.util.Collections;
-import java.util.jar.Attributes;
 
 @RequiredArgsConstructor
 @Service
