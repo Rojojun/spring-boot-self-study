4076101454dd91deda88adef27e17415eab4ccdd
rojojun <zoloman316@gmail.com>
2023-06-08 21:28:50 +0900

    LoginUser 어노테이션 구현 완료

---
+++ src/main/java/com/rojojun/sprignbootweb/config/WebConfig.java
@@ -0,0 +1,20 @@
+package com.rojojun.sprignbootweb.config;
+
+import com.rojojun.sprignbootweb.config.auth.LoginUserArgumentResolver;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    private final LoginUserArgumentResolver loginUserArgumentResolver;
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+        argumentResolvers.add(loginUserArgumentResolver);
+    }
+}
